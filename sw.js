4c45cad7a3c3c268d7f66f87c4a5d385813cd059
wissemlink-oss <wissemlink@gmail.com>
2026-06-26 21:37:29 +0200

    Update sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = "parent-app-v1";
+const URLS = [
+  ".",
+  "icons/icon-192x192.png",
+  "icons/icon-512x512.png",
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(URLS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
